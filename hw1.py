f8907412ad22a4ae00bf2f0a2d06a87918a99ba1
kimdohoe <79958668+kimdohoe@users.noreply.github.com>
2021-03-10 17:37:01 +0900

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+r=0.5
+f0=0.5
+for n in range (1,10):
+    f1=r*f0*(1-f0)
+    f0=f1
+    print(f1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
